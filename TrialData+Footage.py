2b645c25ae47a999755a578167615c3210367075
AidanForrester <aidanaforrester@gmail.com>
2025-10-25 16:36:29 -0400

    Update TrialData+Footage.py
    Added Left and Right Moisture Graph Creation After Trials

--- TrialData+Footage.py
+++ TrialData+Footage.py
@@ -6,6 +6,9 @@ from picamzero import Camera
 from datetime import datetime
 import os
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 i2c_bus = board.I2C()
 ss1 = Seesaw(i2c_bus, addr=XXXX)
 ss2 = Seesaw(i2c_bus, addr=XXXX)
@@ -41,3 +44,42 @@ path = "Desktop/" + current_time + "moisturedata.txt"
 with open(path,'w') as f:
     f.write('\n'.join("end"))
 
+##Right Side Moisture/Time Graph
+fig, mg1 = plt.subplots()
+
+with open(current_time + "moisturedata.txt", "r") as file_object:
+    computingdata = file_object.readlines()
+
+m1 = []
+
+for i in computingdata:
+    if i % 3 == 0:
+        m1.append(i)
+
+s = np.arrange(range(0, 5.18, 0.2))
+t = np.arrange(int(m1))
+
+mg1.plot(s,t)
+mg1.set(xlabel="Time (s)", ylabel="Right Moisture Sensor Reading (mV)", title="Right Moisture Sensor Readings Over Time")
+mg1.grid()
+fig.savefig(current_time + "rmoisturegraph.png")
+
+##Left Side Moisture/Time Graph
+fig, mg2 = plt.subplots()
+
+with open(current_time + "moisturedata.txt", "r") as file_object:
+    computingdata1 = file_object.readlines()
+
+m2 = []
+
+for i in computingdata1:
+    if i % 3 == 1:
+        m2.append(i)
+
+s = np.arrange(range(0, 5.18, 0.2))
+t = np.arrange(int(m2))
+
+mg2.plot(s,t)
+mg2.set(xlabel="Time (s)", ylabel="Right Moisture Sensor Reading (mV)", title="Left Moisture Sensor Readings Over Time")
+mg2.grid()
+fig.savefig(current_time + "lmoisturegraph.png")
