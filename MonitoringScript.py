beae43f5f6f907660e914941aaae8ee7fd71c436
AidanForrester <aidanaforrester@gmail.com>
2025-10-29 21:27:11 -0400

    Update MonitoringScript.py
    Debugged logic, implemented Flask communication and threading

--- MonitoringScript.py
+++ MonitoringScript.py
@@ -2,10 +2,15 @@ import ADS1x15
 import adafruit_ccs811
 from adafruit_bme280 import basic as adafruit_bme280
 from picamzero import Camera
-
+from flask import Flask
 import digitalio
+import board
 from datetime import datetime
 import os
+import threading
+import time
+
+app = Flask(__name__) ##Initialize Flask Framework to Communicate with Webserver
 
 current_time = datetime.now() ##Setup for systematic photo system
 photo_time = datetime.now()
@@ -15,6 +20,7 @@ ccs811 = adafruit_ccs811.CCS811(i2c_bus, address=0x5B)
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c_bus, address=0X77)
 ADS = ADS1x15.ADS1115(1, 0X48)
 bme280.sea_level_pressure = 1013.25
+cam = Camera()
 
 f = ADS.toVoltage() ##Set parameters for Moisture sensors
 ADS.setComparatorThresholdLow( 1.5 / f )
@@ -36,41 +42,80 @@ errorreset = 0
 systemhealthy = 1
 
 signalstart = digitalio.DigitalInOut(board.GP4) ##Initialize starting signal pin
+signalstart.direction = digitalio.Direction.INPUT
+signalstart.pull = digitalio.Pull.UP 
 
-while signalstart == 0: ##While a trial is not happening
-  current_time = datetime.now() ##Read sensors and time
-  moistr = ADS.readADC(0)
-  moistl = ADS.readADC(1)
-  acctemp = bme280.temperature
-  errorcheck = leftlowwater + lefthighwater + rightlowwater + righthighwater + lowtemp + hightemp ##Checks if the system has any errors
-  if current_time - photo_time == 12 ##If it has been 12 hours since the last photo
-    photo_time = current_time ##Take a photo and annotate it
-    cam.annotate(photo_time + " - Internal Temperature: " + bme280.temperataure + " °C - Relative Humidity: " + bme280.relative_humidity + "% - Internal Co2:" + ccs811.eco2 + " ppm")
-    cam.take_photo(f"/Desktop/PlantMonitoring/" + photo_time + ".jpg")
-  if moist1 <= 2##If moisture sensors fall out of expected, send an error
-    leftlowwater = 1
-    ##Add message on web ui to check left sensor and water supply - this should not happen often
-  if moistr <= 2
-    rightlowwater = 1
-    ##Add message on web ui to check right sensor and water supply - this should not happen often
-  if moistl >= 2.75
-    lefthighwater = 1
-    ##Add message on web ui to check left sensor/overall condition of lab - this should not happen often
-  if moistr >= 2.75
-    righthighwater = 1
-    ##Add message on web ui to check left sensor/overall condition of lab - this should not happen often
-  if acctemp >= expectedtemp + 5  ##If temperature falls out of expected, send an error
-    hightemp = 1
-  if acctemp <= expectedtemp - 5 
-    lowtemp = 1
-  if errorreset == 1 ##If an error reset occers, clear all codes
-    leftlowwater = 0
-    lefthighwater = 0
-    rightlowwater = 0
-    righthighwater = 0
-    lowtemp = 0
-    hightemp = 0
-  if errorcheck != 0
-    systemhealthy = 1
-  else 
-    systemhealthy = 0
+@app.route("/temp")
+def temp():
+  return f"<p>{bme280.temperature:.2f} °C</p>"
+
+@app.route("/lmoist")
+def lmoist():
+  return f"<p>{moistl:.2f} V (1.5-3)</p>"
+
+@app.route("/rmoist")
+def rmoist():
+  return f"<p>{moistr:.2f} V (1.5-3)</p>"
+
+@app.route("/eco2")
+def co2():
+  return f"<p>{ccs811.eco2:.2f} °C</p>"
+
+@app.route("/health")
+def systemhealth():
+  if (systemhealthy == 1):
+    return "<p>System Healthy</p>"
+  else:
+    return "<p>Check System!</p>"
+
+def monitor_loop():
+  global moistl, moistr, acctemp, leftlowwater, lefthighwater, rightlowwater, righthighwater, lowtemp, hightemp, errorreset, systemhealthy, photo_time
+  while True:
+    while signalstart.value:
+      time.sleep(.5)
+      continue
+
+    while not signalstart.value: ##While a trial is not happening
+      current_time = datetime.now() ##Read sensors and time
+      moistr = ADS.readADC(0)
+      moistl = ADS.readADC(1)
+      acctemp = bme280.temperature
+      current_time = datetime.now()
+      errorcheck = leftlowwater + lefthighwater + rightlowwater + righthighwater + lowtemp + hightemp ##Checks if the system has any errors
+      if (current_time - photo_time).seconds >= 12 * 3600: ##If it has been 12 hours since the last photo
+          photo_time = current_time ##Take a photo and annotate it
+          cam.annotate(f"{photo_time} - Internal Temperature: {bme280.temperature:.2f} °C - Relative Humidity: {bme280.relative_humidity:.2f}% - Internal CO2: {ccs811.eco2} ppm")
+          cam.take_photo(f"/Desktop/PlantMonitoring/" + photo_time + ".jpg")
+      if (moistl <= 2):##If moisture sensors fall out of expected, send an error
+        leftlowwater = 1
+        ##Add message on web ui to check left sensor and water supply - this should not happen often
+      if (moistr <= 2):
+        rightlowwater = 1
+        ##Add message on web ui to check right sensor and water supply - this should not happen often
+      if (moistl >= 2.75):
+        lefthighwater = 1
+        ##Add message on web ui to check left sensor/overall condition of lab - this should not happen often
+      if (moistr >= 2.75):
+        righthighwater = 1
+        ##Add message on web ui to check left sensor/overall condition of lab - this should not happen often
+      if (acctemp >= expectedtemp + 5):  ##If temperature falls out of expected, send an error
+        hightemp = 1
+      if (acctemp <= expectedtemp - 5):
+        lowtemp = 1
+      if (errorreset == 1): ##If an error reset occers, clear all codes
+        leftlowwater = 0
+        lefthighwater = 0
+        rightlowwater = 0
+        righthighwater = 0
+        lowtemp = 0
+        hightemp = 0
+      if (errorcheck >= 1):
+        systemhealthy = 0
+      else:
+        systemhealthy = 1
+      time.sleep(5) ##Sleep so the CPU does not run at 100%
+    
+if __name__ == "__main__":
+    t = threading.Thread(target=monitor_loop, daemon=True)
+    t.start()
+    app.run(host="0.0.0.0", port=5000)
