32b911d9a45e313b11b6fb2266e6b2d5c93aadca
AidanForrester <aidanaforrester@gmail.com>
2025-10-30 19:26:20 -0400

    Update InstallScript.sh

--- InstallScript.sh
+++ InstallScript.sh
@@ -6,4 +6,4 @@ sudo apt install -y python3-pip python3-picamzero
 python3 -m venv venv
 source venv/bin/activate
 pip install -U pip
-pip install Adafruit-Blinka adafruit-circuitpython-ccs811 adafruit-circuitpython-bme280 adafruit-ads1x15 Flask
+pip install Adafruit-Blinka adafruit-circuitpython-ccs811 adafruit-circuitpython-bme280 ADS1x15-ADC Flask
