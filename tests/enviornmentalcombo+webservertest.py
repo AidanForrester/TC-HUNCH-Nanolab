8713bab2e0cbdd7a3d0e02b50c1412c9d546c219
AidanForrester <aidanaforrester@gmail.com>
2025-11-11 10:37:00 -0500

    Implement error handling for VOC sensor data
    Added error handling for VOC measurement to retain last known value in case of failure.

--- tests/enviornmentalcombo+webservertest.py
+++ tests/enviornmentalcombo+webservertest.py
@@ -32,12 +32,15 @@ maxm2 = 3.05775 ##TEST AND CHANGE THESE
 def sensor_data():
     humidity = round(bme680.humidity, 1)
     temperature = round(bme680.temperature, 1)
-    voc = round(bme680.gas, 1)
+    try:
+     voc = round(bme680.gas, 1)
+     lastvoc = voc
+    except Exception as e:
+     voc = lastvoc
     moist1 = round(((m1.voltage - maxm1) / (minm1 - maxm1)) * 100, 1)
     moist2 = round(((m2.voltage - maxm2) / (minm2 - maxm2)) * 100, 1)
     TDS = round(tds.value, 1)
-    pH = round(ph.value, 1)
-    return jsonify({'humidity': humidity, 'temperature': temperature, 'voc': voc, 'moist1': moist1, 'moist2': moist2, 'tds': TD>
+    return jsonify({'humidity': humidity, 'temperature': temperature, 'VOC': voc, 'moist1': moist1, 'moist2': moist2, 'tds': TD>
 
 @app.route('/', defaults={'path': 'index.html'})
 @app.route('/<path:path>')
