5b49c5cc0017e18deef73b5a72799b754a0a01be
AidanForrester <aidanaforrester@gmail.com>
2025-11-13 20:27:37 -0500

    Update InstallScript.sh

--- InstallScript.sh
+++ InstallScript.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 sudo apt update
 
-sudo apt install -y python3-pip python3-picamzero
-sudo apt install -y libcamera-apps libcamera-tools
-
 python3 -m venv venv
 source venv/bin/activate
 pip install -U pip
-pip install Adafruit-Blinka adafruit-circuitpython-bme680 ADS1x15-ADC Flask
+pip install Adafruit-Blinka adafruit-circuitpython-bme680 adafruit-circuitpython-ads1x15 opencv-python Flask
