22148d25b12f2a193e565f02d238b769ce0cad37
AidanForrester <aidanaforrester@gmail.com>
2025-11-13 20:28:29 -0500

    Update InstallScript.sh

--- InstallScript.sh
+++ InstallScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 
-python3 -m venv venv
-source venv/bin/activate
+python3 -m venv labvenv
+source labvenv/bin/activate
 pip install -U pip
 pip install Adafruit-Blinka adafruit-circuitpython-bme680 adafruit-circuitpython-ads1x15 opencv-python Flask
