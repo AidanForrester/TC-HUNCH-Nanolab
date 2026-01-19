5783cf648991384f4ae4f32b0b8ba4bc21fcf0d1
AidanForrester <aidanaforrester@gmail.com>
2026-01-18 21:40:50 -0500

    Add RPi.GPIO to installation script

--- InstallScript.sh
+++ InstallScript.sh
@@ -4,4 +4,4 @@ sudo apt update
 python3 -m venv labvenv
 source labvenv/bin/activate
 pip install -U pip
-pip install Adafruit-Blinka adafruit-circuitpython-bme680 adafruit-circuitpython-ads1x15 opencv-python Flask
+pip install Adafruit-Blinka adafruit-circuitpython-bme680 adafruit-circuitpython-ads1x15 opencv-python Flask RPi.GPIO
