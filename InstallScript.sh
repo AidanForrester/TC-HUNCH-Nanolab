23d4fa976a307794792c578994ad220a1a99a378
AidanForrester <aidanaforrester@gmail.com>
2026-01-18 21:49:01 -0500

    Update InstallScript.sh

--- InstallScript.sh
+++ InstallScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 
-python3 -m venv labvenv
+python3.11 -m venv labvenv
 source labvenv/bin/activate
 pip install -U pip
-pip install Adafruit-Blinka adafruit-circuitpython-bme680 adafruit-circuitpython-ads1x15 opencv-python Flask RPi.GPIO
+pip install Adafruit-Blinka adafruit-circuitpython-bme680 adafruit-circuitpython-ads1x15 opencv-python Flask RPi.GPIO adafruit-circuitpython-neopixel rpi_ws281x
